bde926040bb9945e78ecdefd45e2bd21b4161b6f
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-12-08 14:40:08 -0500

    comments added

--- src/demos/demo1.py
+++ src/demos/demo1.py
@@ -18,7 +18,59 @@ class TreeNode:
         self.right = right
 
 def inorder_traversal(root):
-    pass
+    # create an inner helper function
+    def helper(root, result):
+        # if root exists
+        if root:
+            # call helper on the left of the root, passing the result list along
+            helper(root.left, result)
+            # in order thing!
+            # append roots value to the result list
+            result.append(root.val, result)
+            # call the helper on the right of the root, passing the result list along
+            helper(root.right)
+    
+    result = []
+    helper(root, result)
+    
+    return result
+
+# iterative solution
+def inorder_traversal(root):
+    # hold the result
+    result = []
+    # make a stack
+    stack = []
+
+    # iterate
+    while True:
+        # while the root node is not none
+        while root: 
+            # append the root to the stack
+            stack.append(root)
+            # traverse to the left of the root
+            root = root.left
+
+            # if there is no stack
+            if not stack:
+                # return the result
+                return result
+            # pop the stack on to a node variable
+            node = stack.pop()
+            # append the nodes value to the result list
+            result.append(node.val)
+            # traverse to the right of the node
+            root = root.right
+
+"""Allision solution"""
+# def inorder_traversal(root):
+#     if not root:
+#         return []
+#     left = inorder_traversal(root.left)
+#     right = inorder_traversal(root.right)
+#     return left + [root.val] + right
+
+
 
 
 t1 = TreeNode(3)
